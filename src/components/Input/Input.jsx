1e94440ed7fc22e49319fcef14e07fcd5ee797ae
Estefck <estef.komu@gmail.com>
2023-11-03 12:15:52 -0400

    value has been added to the component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,12 @@
 import "./Input.scss"
 
-export default function Input({ type, onChangeHandler, placeholder, label }) {
+export default function Input({ type, onChangeHandler, placeholder, label, value }) {
     <div>
         <label>{label}</label>
         <input type={type}
             placeholder={placeholder}
             onChange={onChangeHandler}
+            value={value}
         />
     </div>
 
