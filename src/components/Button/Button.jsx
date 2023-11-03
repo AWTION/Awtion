743f661d472e4e16629544e080b4682bc635fa41
mandeepharsh <harshman47@gmail.com>
2023-11-03 12:27:11 -0400

    add types and functionality for onclick on button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,9 @@
 
 import "./Button.scss"
-const Button = ({icon, label}) => {
+const Button = ({icon, label, variant ,type, ...others }) => {
   return (
-   <button className="Button">
-    <img src={icon}/> 
+   <button className="button" type={type || "button"} {...others}>
+    <img src={icon} /> 
     {label}
    </button>
   )
