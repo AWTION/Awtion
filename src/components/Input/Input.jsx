5291dffbe92c456f8843185097dd9df0f031028d
Estefck <estef.komu@gmail.com>
2023-11-05 14:55:49 -0500

    First draft for the signup form including handling state

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,14 +1,15 @@
 import "./Input.scss"
 
 export default function Input({ type, onChangeHandler, placeholder, label, value, name }) {
-  
-  return(<div>
-        <label>{label}</label>
-        <input type={type}
-            placeholder={placeholder}
-            onChange={onChangeHandler}
-            value={value}
-            name={name}
-        />
+
+    return (<div>
+        <label>{label}
+            <input type={type}
+                placeholder={placeholder}
+                onChange={onChangeHandler}
+                value={value}
+                name={name}
+            />
+        </label>
     </div>)
 }
