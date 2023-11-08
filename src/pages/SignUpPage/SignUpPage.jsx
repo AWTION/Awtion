2e8a8bb584757ac75890224689c030b545cd4e11
Estefck <estef.komu@gmail.com>
2023-11-08 12:07:19 -0500

    added validation for singupform

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -2,6 +2,8 @@ import "./SignUpPage.scss"
 import Input from "../../components/input/Input"
 import { useState } from "react"
 import Button from "../../components/Button/Button"
+import { signUpFormValidation } from "../../utils/validator"
+
 
 export default function () {
 
@@ -12,7 +14,15 @@ export default function () {
         confirmPassword: "",
     }
 
+    const errorState = {
+        username: false,
+        email: false,
+        password: false,
+        confirmPassword: false,
+    }
+
     const [signUpValues, setSignUpValues] = useState(initialSignUpValues)
+    const [error, setError] = useState(errorState)
 
     const onChangeHandler = (event) => {
         const { name, value } = event.target
@@ -22,10 +32,20 @@ export default function () {
             [name]: value,
         })
         console.log(signUpValues);
+
+        setError({
+            ...error,
+            [name]: false,
+        })
+    }
+
+    const onSumbitHandler = (e) => {
+        e.preventDefault();
+        signUpFormValidation();
     }
 
     return (
-        <form className="signup__form">
+        <form className="signup__form" onSubmit={onSumbitHandler}>
             <h2>Sign Up</h2>
             <div>Create a new account</div>
             <Input
@@ -35,6 +55,8 @@ export default function () {
                 placeholder="Username"
                 value={signUpValues.username}
                 onChangeHandler={onChangeHandler}
+                error={error.username}
+                errorMesage={"Incorrect Username"}
             />
             <Input
                 label="Email"
@@ -43,6 +65,8 @@ export default function () {
                 placeholder="Email Address"
                 value={signUpValues.email}
                 onChangeHandler={onChangeHandler}
+                error={error.email}
+                errorMesage={"Incorrect email"}
             />
             <Input
                 label="Password"
@@ -51,6 +75,9 @@ export default function () {
                 placeholder="Password"
                 value={signUpValues.password}
                 onChangeHandler={onChangeHandler}
+                error={error.password}
+                errorMesage={<ul>Password requires :<li>1 special character</li><li>1 upper case</li>
+                    <li>8 to 10 characters long</li></ul>}
             />
             <Input
                 label="Confirm Password"
@@ -59,6 +86,8 @@ export default function () {
                 placeholder="Confirm Password"
                 value={signUpValues.confirmPassword}
                 onChangeHandler={onChangeHandler}
+                error={error.confirmPassword}
+                errorMesage={"Passwords don't match"}
             />
             <div>
                 <Input
@@ -67,9 +96,9 @@ export default function () {
                 <div>By creating an account you have to agree with our terms and conditions.</div>
             </div>
 
-            <Button 
-            type= "Submit"
-            label="Login" />
+            <Button
+                type="Submit"
+                label="Login" />
         </form>
     )
 }
--- src/utils/validator.jsx
+++ src/utils/validator.jsx
@@ -3,7 +3,7 @@ import validator from 'validator';
 
 export function passwordValidation(password) {
     // Check if the password meets the length criteria (8 to 20 characters)
-    if (password.length < 8 || password.length > 20) {
+    if (password.length < 8 || password.length > 15) {
         return false;
     }
 
@@ -21,18 +21,33 @@ export function passwordValidation(password) {
     return true;
 }
 
-export function loginFormValidation(formData){
-    return({
-        email:!validator.isEmail(formData.email),
-        password:passwordValidation(formData.password),
+export function usernameValidation(username) {
+    if (username.length < 8 || username.length > 15) {
+        return false;
+    }
+    return true;
+}
+
+export function verifyPassword(password, confirmPassword) {
+    if (password === confirmPassword) {
+        return false
+    }
+    return true;
+}
+
+
+export function loginFormValidation(formData) {
+    return ({
+        email: !validator.isEmail(formData.email),
+        password: passwordValidation(formData.password),
     })
 }
 
-export function signUpFormValidation(formData){
-return ({
-    username:
-    email:
-    password:
-    confirmPassword:
-})
+export function signUpFormValidation(formData) {
+    return ({
+        username: validator.isEmpty(formData.username) && usernameValidation(),
+        email: !validator.isEmail(formData.email),
+        password: passwordValidation(formData.password),
+        confirmPassword: verifyPassword(),
+    })
 }
